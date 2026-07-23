dffc96d28cf29e26589c99b37711f1ed28d89622
June <zkitun@gmail.com>
2026-07-23 17:08:29 +0800

    feat: 代码优化

--- server/service/system/sys_user.go
+++ server/service/system/sys_user.go
@@ -111,6 +111,9 @@ func (userService *UserService) GetUserInfoList(ctx context.Context, info system
 	if info.Email != "" {
 		db = db.Where("email LIKE ?", "%"+info.Email+"%")
 	}
+	if info.Enable != 0 {
+		db = db.Where("enable = ?", info.Enable)
+	}
 
 	err = db.Count(&total).Error
 	if err != nil {
